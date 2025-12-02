87c0901682b52b8c8010bc0d448b537bec659238
prem singh <premrathore654@gmail.com>
2025-12-02 13:03:34 +0530

    main branch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,30 @@
-// import { defineConfig, globalIgnores } from "eslint/config";
-// import nextVitals from "eslint-config-next/core-web-vitals";
-// import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-// const eslintConfig = defineConfig([
-//   ...nextVitals,
-//   ...nextTs,
-//   // Override default ignores of eslint-config-next.
-//   globalIgnores([
-//     // Default ignores of eslint-config-next:
-//     ".next/**",
-//     "out/**",
-//     "build/**",
-//     "next-env.d.ts",
-//   ]),
-//   {
-//        rules: {
-//       "no-console": ["error", { allow: ["warn", "error"] }],
-
-//       "@typescript-eslint/no-explicit-any": "error",
-
-//       "@typescript-eslint/no-unused-vars": ["error"],
-//       "react-hooks/rules-of-hooks": "error",
-//       "react-hooks/exhaustive-deps": "warn",
-//       ...nextPlugin.configs.recommended.rules,
-//     },
-//   }
-// ]);
-
-// export default eslintConfig;
-
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   {
-    rules: {
-      "no-console": "warn",
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
+       rules: {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+
+      "@typescript-eslint/no-explicit-any": "error",
+
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      ...nextPlugin.configs.recommended.rules,
     },
-  },
-];
+  }
+]);
 
 export default eslintConfig;
