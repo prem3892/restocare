9f93d695ded2b5abccaf0b18e18c01ab75b5f140
prem singh <premrathore654@gmail.com>
2025-12-02 13:33:09 +0530

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,58 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import next from "eslint-config-next";
-import nextPlugin from "@next/eslint-plugin-next";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig([
-  ...next,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  globalIgnores([".next/**", "out/**", "build/**"]),
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-
-    plugins: {
-      "@typescript-eslint": tseslint,
-      next: nextPlugin,
-    },
-
     rules: {
-      "no-console": ["error", { allow: ["warn", "error"] }],
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      ...nextPlugin.configs.recommended.rules,
+      "no-console": "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
     },
   },
-]);
+];
+
+export default eslintConfig;
+
+
+// import { defineConfig, globalIgnores } from "eslint/config";
+// import next from "eslint-config-next";
+// import nextPlugin from "@next/eslint-plugin-next";
+// import tseslint from "@typescript-eslint/eslint-plugin";
+// import tsParser from "@typescript-eslint/parser";
+
+// export default defineConfig([
+//   ...next,
+
+//   globalIgnores([".next/**", "out/**", "build/**"]),
+
+//   {
+//     files: ["**/*.{js,jsx,ts,tsx}"],
+
+//     languageOptions: {
+//       parser: tsParser,
+//       ecmaVersion: "latest",
+//       sourceType: "module",
+//     },
+
+//     plugins: {
+//       "@typescript-eslint": tseslint,
+//       next: nextPlugin,
+//     },
+
+//     rules: {
+//       "no-console": ["error", { allow: ["warn", "error"] }],
+//       "@typescript-eslint/no-explicit-any": "error",
+//       "@typescript-eslint/no-unused-vars": "error",
+//       ...nextPlugin.configs.recommended.rules,
+//     },
+//   },
+// ]);
