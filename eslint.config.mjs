62b5fbbffeed049a98b110e76fb96de686b6f4c7
prem singh <premrathore654@gmail.com>
2025-12-02 13:07:20 +0530

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+
   {
-       rules: {
+    rules: {
       "no-console": ["error", { allow: ["warn", "error"] }],
-
       "@typescript-eslint/no-explicit-any": "error",
-
-      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-unused-vars": "error",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
+
       ...nextPlugin.configs.recommended.rules,
     },
-  }
+  },
 ]);
-
-export default eslintConfig;
