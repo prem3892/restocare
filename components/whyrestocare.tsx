a182f300a287a16a534d6fcefe765ed37f26b5c7
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-12-05 16:08:33 +0530

    why restocare component fix

--- components/whyrestocare.tsx
+++ components/whyrestocare.tsx
@@ -8,12 +8,12 @@ import { Typography } from '@/typography/typography';
 export const WhyRestoCare = () => {
   return (
     <section
-      className="flex min-h-screen sm:flex-row flex-col
-      justify-center items-center
+      className="flex min-h-screen sm:flex-row flex-col gap-[clamp(8px,10vw,32px)]
+      justify-center items-center not-sm:
     bg-white
     "
     >
-      <div className="why-resto-left flex-1 flex justify-start items-center max-w-3/7">
+      <div className="why-resto-left flex-1 flex justify-start items-center max-w-full sm:max-w-3/7">
         <div className="flex flex-col gap-[clamp(8px,2vw,24px)] relative w-fit h-fit">
           <Image
             src={whyrestoimg1}
@@ -34,7 +34,7 @@ export const WhyRestoCare = () => {
         </div>
       </div>
 
-      <div className="why-resto-right max-w-3/7 flex flex-1 flex-col gap-[clamp(8px,1vw,24px)] justify-center items-center">
+      <div className="why-resto-right max-w-8/10 sm:max-w-3/7 flex flex-1 flex-col gap-[clamp(8px,1vw,24px)] justify-center items-center">
         <Typography
           text={WhyRestoCareData.heading}
           element="h2"
