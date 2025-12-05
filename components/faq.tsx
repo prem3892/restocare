55189f922d15dc16050015957c88335e44bf8ab6
prem singh <premrathore654@gmail.com>
2025-12-05 16:27:05 +0530

    ft:faq updated

--- components/faq.tsx
+++ components/faq.tsx
@@ -2,34 +2,7 @@
 
 import { useState } from 'react';
 import { ChevronDown } from 'lucide-react';
-
-type FaqItem = {
-  question: string;
-  answer: string;
-};
-
-const faqs: FaqItem[] = [
-  {
-    question: 'Q1: What is RestoCare?',
-    answer:
-      'A1: RestoCare is a comprehensive restaurant management solution designed to streamline operations, enhance customer experience, and boost overall efficiency in the food service industry.',
-  },
-  {
-    question: 'Q1: What is RestoCare?',
-    answer:
-      'A1: RestoCare is a comprehensive restaurant management solution designed to streamline operations, enhance customer experience, and boost overall efficiency in the food service industry.',
-  },
-  {
-    question: 'Q1: What is RestoCare?',
-    answer:
-      'A1: RestoCare is a comprehensive restaurant management solution designed to streamline operations, enhance customer experience, and boost overall efficiency in the food service industry.',
-  },
-  {
-    question: 'Q1: What is RestoCare?',
-    answer:
-      'A1: RestoCare is a comprehensive restaurant management solution designed to streamline operations, enhance customer experience, and boost overall efficiency in the food service industry.',
-  },
-];
+import { faqs } from '@/variant/faq.variant';
 
 function Faq() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
