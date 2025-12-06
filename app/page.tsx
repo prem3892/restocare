42ddd2d0ef70b93eb00d356bae9d1f36bc45f495
prem singh <premrathore654@gmail.com>
2025-12-06 13:25:44 +0530

    developer updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,20 @@ import { Carousel } from '../components/carousel';
 import { Navbar } from '../components/header';
 import { WhyRestoCare } from '@/components/whyrestocare';
 import { HowItWorks } from '@/components/howitworks';
+import Testimonial from '@/components/testimonial';
+import Faq from '@/components/faq';
+import Blog from '@/components/blog';
 
 export default function Home() {
   return (
     <>
-      {/* <nav className="z-50 fixed top-0 w-screen"> */}
       <Navbar />
-      {/* </nav> */}
       <Carousel />
       <WhyRestoCare />
       <HowItWorks />
-      {/* <Testimonial /> */}
+      <Testimonial />
+      <Faq />
+      <Blog />
       <div className="p-6">hello next app</div>
     </>
   );
