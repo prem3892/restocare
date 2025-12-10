5dbb4122bab58aa1e22db26121d0c3f7bc0d8d1e
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-12-10 13:09:07 +0530

    added navigation and routing support for about us page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ const Testimonial = dynamic(() => import('@/components/testimonial'));
 const Faq = dynamic(() => import('@/components/faq'));
 const Blog = dynamic(() => import('@/components/blog'), { ssr: true });
 const Subfooter = dynamic(() => import('@/components/subfooter'), { ssr: true });
-import { OurCompany } from '@/components/ourcompany';
+// import { OurCompany } from '@/app/aboutus/ourcompany';
 import { LetsTalk } from '@/components/letstalk';
-import { OurClients } from '@/components/ourclients';
+// import { OurClients } from '@/components/ourclients';
 import { NeedSupport } from '@/components/needsupport';
 import Video from '@/components/video';
 import dynamic from 'next/dynamic';
@@ -24,11 +24,11 @@ export default function Home() {
       <Testimonial />
       <Faq />
       <Blog />
+      <LetsTalk />
       <NeedSupport />
       <Subfooter />
-      <OurCompany />
-      <LetsTalk />
-      <OurClients />
+      {/* <OurCompany /> */}
+      {/* <OurClients /> */}
     </>
   );
 }
